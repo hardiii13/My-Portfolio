54cfbca3b08e8658f91196c0d3da2c7a3f35cf62
Hardik <hardik.gupta1173@gmail.com>
2022-08-24 13:01:09 +0530

    First commit

---
+++ sanity-backend/schemas/author.js
@@ -0,0 +1,23 @@
+export default {
+  name: 'author',
+  type: 'document',
+	title: 'Author',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title'
+    },
+      {
+      name: 'desc',
+      type: 'string',
+      title: 'Description'
+    },
+     {
+      name: 'image',
+      type: 'image',
+      title: 'Image'
+    },
+    
+]
+}
